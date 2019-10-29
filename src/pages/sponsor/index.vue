03b46d35e51261672b8f58350f830de7ffa964b9
madong <madong@1m2.net>
2019-10-29 16:45:04 +0800

    新增赞助商列表页

---
+++ src/pages/sponsor/index.vue
@@ -0,0 +1,155 @@
+<template>
+  <div>
+    <div class="sm-width-100-per  sm-margin-bottom-20 sm-layout-center-vertical"
+         style="height: 400rpx;">
+      <div class="sm-width-90-per sm-height-90-per card"
+           style="position: relative;">
+        <image mode=""
+               :src="imgtest"
+               class="image-size" />
+        <div class="ms-layout-wrap-vertical sponsor-detail"
+             style="border-radius: 0.25rem;width: 540rpx;height: 120rpx;position:Absolute;bottom: 40rpx;left: 20rpx;background: rgba(0, 0, 0, 0.5);">
+          <div class="opacity-1 sm-width-90-per sm-height-40-per sm-layout-left-center sponsor-detail">桥头堡火锅</div>
+          <div class="opacity-1 sm-width-90-per sm-height-60-per sm-layout-left-center sponsor-detail">四川省成都市锦江区 走马街68号,锦城大厦轩雅映月酒店2层 时光盒子网咖(春熙路店)</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="sm-width-100-per  sm-margin-bottom-20 sm-layout-center-vertical"
+         style="height: 400rpx;">
+      <div class="sm-width-90-per sm-height-90-per card"
+           style="position: relative;">
+        <image mode=""
+               :src="imgtest"
+               class="image-size" />
+        <div class="ms-layout-wrap-vertical sponsor-detail"
+             style="border-radius: 0.25rem;width: 540rpx;height: 120rpx;position:Absolute;bottom: 40rpx;left: 20rpx;background: rgba(0, 0, 0, 0.5);">
+          <div class="opacity-1 sm-width-90-per sm-height-40-per sm-layout-left-center sponsor-detail">桥头堡火锅</div>
+          <div class="opacity-1 sm-width-90-per sm-height-60-per sm-layout-left-center sponsor-detail">四川省成都市锦江区 走马街68号,锦城大厦轩雅映月酒店2层 时光盒子网咖(春熙路店)</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="sm-width-100-per  sm-margin-bottom-20 sm-layout-center-vertical"
+         style="height: 400rpx;">
+      <div class="sm-width-90-per sm-height-90-per card"
+           style="position: relative;">
+        <image mode=""
+               :src="imgtest"
+               class="image-size" />
+        <div class="ms-layout-wrap-vertical sponsor-detail"
+             style="border-radius: 0.25rem;width: 540rpx;height: 120rpx;position:Absolute;bottom: 40rpx;left: 20rpx;background: rgba(0, 0, 0, 0.5);">
+          <div class="opacity-1 sm-width-90-per sm-height-40-per sm-layout-left-center sponsor-detail">桥头堡火锅</div>
+          <div class="opacity-1 sm-width-90-per sm-height-60-per sm-layout-left-center sponsor-detail">四川省成都市锦江区 走马街68号,锦城大厦轩雅映月酒店2层 时光盒子网咖(春熙路店)</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="sm-width-100-per  sm-margin-bottom-20 sm-layout-center-vertical"
+         style="height: 400rpx;">
+      <div class="sm-width-90-per sm-height-90-per card"
+           style="position: relative;">
+        <image mode=""
+               :src="imgtest"
+               class="image-size" />
+        <div class="ms-layout-wrap-vertical sponsor-detail"
+             style="border-radius: 0.25rem;width: 540rpx;height: 120rpx;position:Absolute;bottom: 40rpx;left: 20rpx;background: rgba(0, 0, 0, 0.5);">
+          <div class="opacity-1 sm-width-90-per sm-height-40-per sm-layout-left-center sponsor-detail">桥头堡火锅</div>
+          <div class="opacity-1 sm-width-90-per sm-height-60-per sm-layout-left-center sponsor-detail">四川省成都市锦江区 走马街68号,锦城大厦轩雅映月酒店2层 时光盒子网咖(春熙路店)</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="sm-width-100-per  sm-margin-bottom-20 sm-layout-center-vertical"
+         style="height: 400rpx;">
+      <div class="sm-width-90-per sm-height-90-per card"
+           style="position: relative;">
+        <image mode=""
+               :src="imgtest"
+               class="image-size" />
+        <div class="ms-layout-wrap-vertical sponsor-detail"
+             style="border-radius: 0.25rem;width: 540rpx;height: 120rpx;position:Absolute;bottom: 40rpx;left: 20rpx;background: rgba(0, 0, 0, 0.5);">
+          <div class="opacity-1 sm-width-90-per sm-height-40-per sm-layout-left-center sponsor-detail">桥头堡火锅</div>
+          <div class="opacity-1 sm-width-90-per sm-height-60-per sm-layout-left-center sponsor-detail">四川省成都市锦江区 走马街68号,锦城大厦轩雅映月酒店2层 时光盒子网咖(春熙路店)</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="sm-width-100-per  sm-margin-bottom-20 sm-layout-center-vertical"
+         style="height: 400rpx;">
+      <div class="sm-width-90-per sm-height-90-per card"
+           style="position: relative;">
+        <image mode=""
+               :src="imgtest"
+               class="image-size" />
+        <div class="ms-layout-wrap-vertical sponsor-detail"
+             style="border-radius: 0.25rem;width: 540rpx;height: 120rpx;position:Absolute;bottom: 40rpx;left: 20rpx;background: rgba(0, 0, 0, 0.5);">
+          <div class="opacity-1 sm-width-90-per sm-height-40-per sm-layout-left-center sponsor-detail">桥头堡火锅</div>
+          <div class="opacity-1 sm-width-90-per sm-height-60-per sm-layout-left-center sponsor-detail">四川省成都市锦江区 走马街68号,锦城大厦轩雅映月酒店2层 时光盒子网咖(春熙路店)</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style  lang="less">
+.card {
+  font-weight: 400;
+  border: 0;
+  // box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  // background-clip: border-box;
+  // border: 1px solid rgba(0, 0, 0, 0.125);
+  border-radius: 0.25rem;
+  background-color: white;
+}
+.sm-margin-bottom-20 {
+  margin-bottom: 20rpx;
+}
+.image-size {
+  width: 100%;
+  height: 100%;
+}
+.sponsor-detail {
+  color: #b9b4b4;
+  font-size: 24rpx;
+}
+.opacity-1 {
+  opacity: 1;
+}
+page {
+  background-color: #e6e5e5;
+}
+</style>
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      imgtest: '../../static/images/im2.jpg'
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    },
+    clickHandle (ev) {
+      console.log('clickHandle:', ev)
+      // throw {message: 'custom test'}
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
